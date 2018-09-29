6476acfcf876dbbb3f6cdad80038a72d45df71e3
William Uchegbu <wcu201@nyu.edu>
2018-09-29 17:34:37 -0400

    Initial Commit

---
+++ Boothe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boothe
+//
+//  Created by William  Uchegbu on 9/29/18.
+//  Copyright © 2018 William  Uchegbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
